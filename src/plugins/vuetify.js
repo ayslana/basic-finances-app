55e6ba314265a6c4365ef56061b2296d0fdda83e
ayslana <ayslanariene@gmail.com>
2023-01-11 19:42:48 -0300

    add vuetify themes

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -3,4 +3,30 @@ import Vuetify from "vuetify/lib/framework";
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+  theme: {
+    dark: false,
+    themes: {
+      light: {
+        primary: "#232B59",
+        secondary: "#5864A6",
+        accent: "#82B1FF",
+        error: "#FF5252",
+        info: "#2196F3",
+        success: "#4CAF50",
+        warning: "#FFC107",
+        background: "#EEEEEE",
+      },
+      dark: {
+        primary: "#F23568",
+        secondary: "#8C2E62",
+        accent: "#82B1FF",
+        error: "#FF5252",
+        info: "#2196F3",
+        success: "#4CAF50",
+        warning: "#FFC107",
+        background: "#EEEEEE",
+      },
+    },
+  },
+});
