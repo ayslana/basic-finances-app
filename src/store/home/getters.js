a83db17f74bfc4f5221317ae94de46cc2ffb4569
ayslana <ayslanariene@gmail.com>
2023-01-12 18:40:12 -0300

    create getters

--- src/store/home/getters.js
+++ src/store/home/getters.js
@@ -1,1 +1,11 @@
-export default {};
+export default {
+  getFinanceData: (state) => state.financeData,
+
+  getBitcoin: (state) => state.bitcoin,
+
+  getCurrencies: (state) => state.currencies,
+
+  getStocks: (state) => state.stocks,
+
+  getIsLoading: (state) => state.isLoading,
+};
