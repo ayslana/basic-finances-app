281046eed5ee5513f7b7e6ddcebea6a70823d8a4
ayslana <ayslanariene@gmail.com>
2023-01-12 18:40:20 -0300

    create mutations

--- src/store/home/mutations.js
+++ src/store/home/mutations.js
@@ -1,1 +1,16 @@
-export default {};
+export default {
+  setFinanceData(state, response) {
+    state.financeData = response;
+    var currenciesMapped = Object.keys(response.currencies).map((key) => {
+      return response.currencies[key];
+    });
+    currenciesMapped = currenciesMapped.slice(1);
+    state.currencies = currenciesMapped;
+    state.bitcoin = response.bitcoin;
+    state.stocks = response.stocks;
+  },
+
+  setIsLoading(state, value) {
+    state.isLoading = value;
+  },
+};
