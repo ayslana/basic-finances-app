b0f162cd1c661b23d89e972353a7fff667207bb2
ayslana <ayslanariene@gmail.com>
2023-01-12 18:50:59 -0300

    create finances table

---
+++ src/components/home/FinancesTable.vue
@@ -0,0 +1,144 @@
+<template>
+  <div class="my-8" v-if="getCurrencies">
+    <v-card-title class="text-h6 text--text"
+      >Câmbio das Moedas
+      <v-col align="end" class="py-0">
+        <v-btn
+          outlined
+          elevation="0"
+          color="primary"
+          class="text-truncate"
+          style="text-transform: none !important; font-weight: bolder"
+          @click="openGenerateReportDialog"
+          >Gerar Relatório
+        </v-btn>
+      </v-col>
+    </v-card-title>
+    <v-data-table
+      :headers="headers"
+      :single-select="singleSelect"
+      :items="dataTableComponent"
+      :items-per-page="9"
+      class="font-weight-bold base"
+    >
+      <template v-slot:[`item.name`]="{ item }">
+        <v-chip
+          color="transparent"
+          class="text-truncate"
+          style="max-width: 130px"
+          @click="openLineChartDialog"
+        >
+          {{ item.name }}
+        </v-chip>
+      </template>
+      <template v-slot:[`item.buy`]="{ item }">
+        <v-chip color="transparent">
+          {{ item.buy }}
+        </v-chip>
+      </template>
+      <template v-slot:[`item.sell`]="{ item }">
+        <v-chip color="transparent">
+          {{ item.sell ? item.sell : "-" }}
+        </v-chip>
+      </template>
+      <template v-slot:[`item.variation`]="{ item }">
+        <v-chip
+          :color="item.variation >= 0 ? 'green' : 'red'"
+          class="white--text"
+        >
+          {{ item.variation }}
+        </v-chip>
+      </template>
+    </v-data-table>
+    <v-dialog
+      v-model="dialogGenerateReport"
+      scrollable
+      max-width="720"
+      transition="dialog-bottom-transition"
+    >
+      <line-chart-dialog
+        :key="reRender"
+        @closeLineChartDialog="closeLineChartDialog"
+      />
+    </v-dialog>
+  </div>
+  <div v-else style="height: 100% !important">
+    <v-progress-circular
+      color="primary"
+      indeterminate
+      size="64"
+      style="height: 100% !important"
+    ></v-progress-circular>
+  </div>
+</template>
+<script>
+import { mapGetters, mapActions, mapState } from "vuex";
+import LineChartDialog from "./LineChartDialog";
+
+export default {
+  name: "FinancesTable",
+  components: {
+    LineChartDialog,
+  },
+  data: () => ({
+    lineChartDialog: false,
+    reRender: 0,
+    red: null,
+    orange: null,
+    headers: [
+      {
+        text: "Moeda",
+        value: "name",
+        sortable: false,
+      },
+      { text: "Compra", value: "buy" },
+      { text: "Venda", value: "sell" },
+      { text: "Variação", value: "variation" },
+    ],
+    singleSelect: false,
+  }),
+  computed: {
+    ...mapGetters("home", ["getCurrencies"]),
+    ...mapState("home", ["currencies"]),
+    dataTableComponent() {
+      return this.getCurrencies;
+    },
+    getDataTable() {
+      return this.dataTableComponent;
+    },
+  },
+  // beforeDestroy() {
+  //   this.$store.commit("home/setFinanceData", []);
+  // },
+  async mounted() {
+    await this.getData();
+  },
+  methods: {
+    ...mapActions("home", ["requestFinanceData"]),
+
+    async getData() {
+      await this.requestFinanceData();
+    },
+
+    openLineChartDialog() {
+      this.lineChartDialog = true;
+      this.reRender += 1;
+    },
+
+    closeLineChartDialog() {
+      this.lineChartDialog = false;
+    },
+  },
+};
+</script>
+<style scoped>
+::v-deep
+  .theme--dark.v-data-table
+  > .v-data-table__wrapper
+  > table
+  > tbody
+  > tr
+  > td {
+  color: white;
+}
+</style>
