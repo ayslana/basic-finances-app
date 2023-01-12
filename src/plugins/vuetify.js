eec55bc4cb0b7863219a624ee76b63cdcaf10ff4
ayslana <ayslanariene@gmail.com>
2023-01-11 23:33:53 -0300

    change primary color

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -8,7 +8,7 @@ export default new Vuetify({
     dark: false,
     themes: {
       light: {
-        primary: "#232B59",
+        primary: "#1A1C40",
         secondary: "#5864A6",
         accent: "#82B1FF",
         error: "#FF5252",
