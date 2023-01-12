a18777c2c6a0cb6a051131c44350e102df9594ac
ayslana <ayslanariene@gmail.com>
2023-01-11 23:26:49 -0300

    create home view

---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container>
+    <v-row align="center">
+      <v-col cols="12">
+        <div class="base_dark pa-4">
+          <finances-table />
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import FinancesTable from "@/components/home/FinancesTable.vue";
+
+export default {
+  name: "DashBoard",
+  components: {
+    FinancesTable,
+  },
+};
+</script>
+<style scoped></style>
