78171e285c8f834c13088d8dd7a70877ee31b7f4
ayslana <ayslanariene@gmail.com>
2023-01-13 20:28:30 -0300

    create login mutations

--- src/store/auth/mutations.js
+++ src/store/auth/mutations.js
@@ -1,1 +1,13 @@
-export default {};
+export default {
+  setToken(state, token) {
+    state.token = token;
+  },
+
+  setUserData(state, payload) {
+    state.userData = payload;
+  },
+
+  setIsLoading(state, payload) {
+    state.isLoading = payload;
+  },
+};
