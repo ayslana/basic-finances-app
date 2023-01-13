33971dfbf708ea3c079bf5ee7955e870022b8e41
ayslana <ayslanariene@gmail.com>
2023-01-13 20:26:10 -0300

    fix responsiveness & add validation methods

--- src/components/register/RegisterDialog.vue
+++ src/components/register/RegisterDialog.vue
@@ -10,60 +10,80 @@
     </v-toolbar>
     <v-card-text class="pt-12">
       <v-row justify="center">
-        <v-col cols="6" align="center" align-self="center">
-          <v-row justify="center" class="pb-12"
-            ><span class="text-h2 login--text font-weight-bold"
-              >WorldWallet</span
-            >
+        <v-col
+          cols="12"
+          sm="12"
+          md="12"
+          lg="6"
+          xl="6"
+          align="center"
+          align-self="center"
+        >
+          <v-row justify="center" class="py-3 mx-3">
+            <v-col cols="12" sm="12" md="8" lg="12" xl="12">
+              ><span class="text-h3 login--text font-weight-bold"
+                >WorldWallet</span
+              >
+            </v-col>
           </v-row>
-          <v-row class="py-3 mx-3">
-            <v-text-field
-              v-model="login.email"
-              label="E-mail"
-              outlined
-              dense
-              required
-              :rules="emailRules"
-            ></v-text-field>
+          <v-row justify="center" class="py-3 mx-3">
+            <v-col cols="12" sm="12" md="8" lg="12" xl="12">
+              <v-text-field
+                v-model="modelRegister.email"
+                label="E-mail"
+                outlined
+                dense
+                required
+                :rules="emailRules"
+              ></v-text-field>
+            </v-col>
           </v-row>
-          <v-row class="py-3 mx-3">
-            <v-text-field
-              v-model="login.password"
-              label="Senha"
-              outlined
-              dense
-              required
-              :rules="passwordRules"
-              :type="showPassword ? 'text' : 'password'"
-              :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-              @click:append="showPassword = !showPassword"
-            ></v-text-field>
+          <v-row justify="center" class="py-3 mx-3">
+            <v-col cols="12" sm="12" md="8" lg="12" xl="12">
+              <v-text-field
+                v-model="modelRegister.password"
+                label="Senha"
+                outlined
+                dense
+                required
+                :rules="passwordRules"
+                :type="showPassword ? 'text' : 'password'"
+                :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                @click:append="showPassword = !showPassword"
+              ></v-text-field>
+            </v-col>
           </v-row>
-          <v-row class="py-3 mx-3">
-            <v-text-field
-              v-model="login.confimPassword"
-              label="Confirmar Senha"
-              outlined
-              dense
-              required
-              :rules="passwordRules"
-              :type="showPassword ? 'text' : 'password'"
-              :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-              @click:append="showPassword = !showPassword"
-            ></v-text-field>
+          <v-row justify="center" class="py-3 mx-3">
+            <v-col cols="12" sm="12" md="8" lg="12" xl="12">
+              <v-text-field
+                v-model="confimPassword"
+                label="Confirmar Senha"
+                outlined
+                dense
+                required
+                :rules="passwordRules"
+                :type="showPassword ? 'text' : 'password'"
+                :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                @click:append="showPassword = !showPassword"
+              ></v-text-field>
+              <span v-show="passwordDivergence" class="primary"
+                >As senhas devem ser iguais.</span
+              >
+            </v-col>
           </v-row>
         </v-col>
       </v-row>
       <v-card-actions>
         <v-container
           ><v-row justify="center" class="py-3 mx-3">
-            <v-col cols="3">
+            <v-col cols="12" sm="12" md="8" lg="3" xl="3">
               <v-btn
                 rounded
                 x-large
                 block
                 class="white--text"
                 style="text-transform: none !important; font-weight: bolder"
+                :disabled="disabledField"
                 :color="hover ? 'secondary' : 'login'"
                 :loading="isLoading"
                 @click="sendRegister()"
@@ -77,19 +97,54 @@
   </v-card>
 </template>
 <script>
+import { mapActions } from "vuex";
+
 export default {
   data: () => ({
     showPassword: false,
-    login: {
+    modelRegister: {
       email: "",
       password: "",
-      confirmPassword: "",
     },
+    confirmPassword: "",
+    passwordDivergence: false,
   }),
-
+  watch: {
+    confirmPassword: {
+      handler(newValue) {
+        newValue.confirmPassword = !this.modelRegister.password
+          ? (this.passwordDivergence = true)
+          : (this.passwordDivergence = false);
+      },
+      immediate: true,
+      deep: true,
+    },
+  },
   methods: {
+    ...mapActions("register", ["register"]),
+
+    sendRegister() {
+      this.modelRegister.password === this.modelRegister.confirmPassword
+        ? this.register
+        : "";
+    },
+
     closeRegisterDialog() {
       this.$emit("closeRegisterDialog");
+    },
+  },
+  computed: {
+    disabledField() {
+      if (
+        this.modelRegister.email === "" ||
+        this.modelRegister.password === "" ||
+        this.modelRegister.confirmPassword === "" ||
+        this.modelRegister.password != this.confirmPassword
+      ) {
+        return true;
+      } else {
+        return false;
+      }
     },
   },
 };
