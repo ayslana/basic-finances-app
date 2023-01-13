581f42f825f9c3dfb60d0f17a8c1de63f6a93cb0
ayslana <ayslanariene@gmail.com>
2023-01-13 20:26:43 -0300

    add auth

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import LoginView from "../views/LoginView.vue";
 import HomeView from "../views/HomeView.vue";
+import store from "@/store/index";
 
 Vue.use(VueRouter);
 
@@ -10,11 +11,25 @@ const routes = [
     name: "Login",
     path: "/",
     component: LoginView,
+    meta: {
+      requiresAuth: false,
+    },
   },
   {
     name: "Home",
     path: "/home",
     component: HomeView,
+    meta: {
+      requiresAuth: true,
+    },
+  },
+  {
+    path: "*",
+    name: "Home",
+    component: HomeView,
+    meta: {
+      requiresAuth: true,
+    },
   },
 ];
 
@@ -24,4 +39,19 @@ const router = new VueRouter({
   routes,
 });
 
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    if (!store.getters["auth/getIsLogged"]) {
+      next({
+        path: "/",
+      });
+    } else {
+      next({
+        path: "/home",
+      });
+    }
+  } else {
+    next();
+  }
+});
 export default router;
