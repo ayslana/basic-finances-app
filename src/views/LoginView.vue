b286f8d3aea0fb609a77afd909f078bb97236ebf
ayslana <ayslanariene@gmail.com>
2023-01-13 18:01:19 -0300

    add register dialog to login view

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -37,28 +37,73 @@
               rounded
               x-large
               block
-              class="white--text"
+              class="white--text login"
               style="text-transform: none !important; font-weight: bolder"
-              :color="hover ? 'secondary' : 'login'"
               :loading="isLoading"
               @click="sendLogin()"
               >Login
             </v-btn>
           </v-col>
         </v-row>
+        <v-row justify="center" class="py-3 mx-3">
+          <v-col cols="6">
+            <span>Não possui Login? </span>
+            <v-hover v-slot="{ hover }">
+              <v-btn
+                block
+                plain
+                text
+                style="text-transform: none !important; font-weight: bolder"
+                :class="hover ? 'accent--text' : 'login--text'"
+                :loading="isLoading"
+                @click="openRegisterDialog()"
+                >Cadastre-se.
+              </v-btn>
+            </v-hover>
+          </v-col>
+        </v-row>
       </v-col>
     </v-row>
+    <v-dialog
+      v-model="dialogRegister"
+      fullscreen
+      hide-overlay
+      scrollable
+      max-width="720"
+      transition="dialog-bottom-transition"
+    >
+      <register-dialog @closeRegisterDialog="closeRegisterDialog" />
+    </v-dialog>
   </v-container>
 </template>
 <script>
+import RegisterDialog from "@/components/register/RegisterDialog.vue";
+
 export default {
+  name: "LoginView",
+  components: {
+    RegisterDialog,
+  },
   data: () => ({
+    dialogRegister: false,
     showPassword: false,
     login: {
       email: "",
       password: "",
     },
   }),
+  methods: {
+    openRegisterDialog() {
+      this.dialogRegister = true;
+    },
+    closeRegisterDialog() {
+      this.dialogRegister = false;
+    },
+  },
 };
 </script>
-<style lang=""></style>
+<style scopped>
+.v-ripple__animation {
+  display: none;
+}
+</style>
