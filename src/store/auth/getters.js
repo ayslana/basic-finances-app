cd53079302f246a2009c10d50d9ac248743b7188
ayslana <ayslanariene@gmail.com>
2023-01-13 20:28:40 -0300

    create login getters

--- src/store/auth/getters.js
+++ src/store/auth/getters.js
@@ -1,1 +1,17 @@
-export default {};
+export default {
+  getIsLogged(state) {
+    return state.isLogged;
+  },
+
+  getToken(state) {
+    return state.token;
+  },
+
+  getUserName(state) {
+    return state.userData.name;
+  },
+
+  getIsLoading(state) {
+    return state.userData.userName;
+  },
+};
