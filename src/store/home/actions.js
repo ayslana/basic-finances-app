2b8ac39fa4119a611fd076c61e830b037e9b3938
ayslana <ayslanariene@gmail.com>
2023-01-14 15:43:23 -0300

    fix key

--- src/store/home/actions.js
+++ src/store/home/actions.js
@@ -2,7 +2,7 @@ import { HomeRepository } from "@/repositories/HomeRepository";
 
 export default {
   async requestFinanceData({ commit }) {
-    const key = "5060e767";
+    const key = "be66649e";
     const response = await HomeRepository.getFinanceData(key);
     if (response.status >= 200 && response.status < 300) {
       commit("setFinanceData", response.data.results);
