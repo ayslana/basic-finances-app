b26cc4bae9c4f6a8a5c68a8c984e5130a4a6fcae
ayslana <ayslanariene@gmail.com>
2023-01-14 14:54:31 -0300

    lint

--- src/repositories/HomeRepository.js
+++ src/repositories/HomeRepository.js
@@ -2,8 +2,6 @@ import { HttpRestService } from "@/services/http/HttpRestService";
 
 export class HomeRepository {
   static getFinanceData(key) {
-    return HttpRestService.get(
-      `/finance?format=json-cors&key=${key}`
-    );
+    return HttpRestService.get(`/finance?format=json-cors&key=${key}`);
   }
 }
