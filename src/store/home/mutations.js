6251c14c8ad35c153897671041279ba9f1e86b12
ayslana <ayslanariene@gmail.com>
2023-01-13 23:13:50 -0300

    add mutations

--- src/store/home/mutations.js
+++ src/store/home/mutations.js
@@ -1,13 +1,21 @@
 export default {
   setFinanceData(state, response) {
     state.financeData = response;
+
     var currenciesMapped = Object.keys(response.currencies).map((key) => {
       return response.currencies[key];
     });
     currenciesMapped = currenciesMapped.slice(1);
     state.currencies = currenciesMapped;
-    state.bitcoin = response.bitcoin;
+
+    var bitcoinMapped = Object.keys(response.bitcoin).map((key) => {
+      return response.bitcoin[key];
+    });
+    bitcoinMapped = bitcoinMapped.slice(1);
+    
+    state.bitcoin = bitcoinMapped;
     state.stocks = response.stocks;
+    state.taxes = response.taxes;
   },
 
   setIsLoading(state, value) {
