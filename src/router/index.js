6ea913f8edf198d49ff66b15b8ea98fc52b3bbee
ayslana <ayslanariene@gmail.com>
2023-01-13 22:17:47 -0300

    add before enter

--- src/router/index.js
+++ src/router/index.js
@@ -2,56 +2,43 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import LoginView from "../views/LoginView.vue";
 import HomeView from "../views/HomeView.vue";
-import store from "@/store/index";
+import NotFoundView from "../views/NotFoundView.vue";
+import store from "@/store/index.js";
 
 Vue.use(VueRouter);
 
-const routes = [
-  {
-    name: "Login",
-    path: "/",
-    component: LoginView,
-    meta: {
-      requiresAuth: false,
-    },
-  },
-  {
-    name: "Home",
-    path: "/home",
-    component: HomeView,
-    meta: {
-      requiresAuth: true,
-    },
-  },
-  {
-    path: "*",
-    name: "Home",
-    component: HomeView,
-    meta: {
-      requiresAuth: true,
-    },
-  },
-];
-
 const router = new VueRouter({
   mode: "history",
-  base: process.env.BASE_URL,
-  routes,
+  base: "",
+  routes: [
+    {
+      name: "Login",
+      path: "/",
+      component: LoginView,
+    },
+    {
+      name: "Home",
+      path: "/home",
+      component: HomeView,
+      beforeEnter: beforeEnter,
+    },
+    {
+      path: "/nao-encontrado",
+      name: "404",
+      component: NotFoundView,
+    },
+    { path: "*", redirect: "/nao-encontrado" },
+  ],
 });
 
-router.beforeEach((to, from, next) => {
-  if (to.matched.some((record) => record.meta.requiresAuth)) {
-    if (!store.getters["auth/getIsLogged"]) {
-      next({
-        path: "/",
-      });
-    } else {
-      next({
-        path: "/home",
-      });
-    }
-  } else {
+function beforeEnter(to, from, next) {
+  if (store.getters["auth/getIsLogged"]) {
     next();
+  } else {
+    next({
+      name: "Login",
+    });
   }
-});
+}
+
 export default router;
